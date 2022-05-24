2a2f155ecfb32c6c9a21bc7e6af8ede26b46be1f
mitchpk <kphctim@gmail.com>
2022-05-24 17:51:09 +0930

    some notes

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,23 @@
-use anyhow::{anyhow, Result};
+use anyhow::{Context, Result};
 use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
+use std::f32::consts::{PI, TAU};
+
+struct Note {
+    freq: f32,
+    clock: f32,
+}
+
+impl From<f32> for Note {
+    fn from(freq: f32) -> Self {
+        Note { freq, clock: 0.0 }
+    }
+}
 
 struct SampleRequestOptions {
     sample_rate: f32,
     sample_clock: f32,
     nchannels: usize,
+    notes: Vec<Note>,
 }
 
 struct SampleRequestData {
@@ -24,13 +37,10 @@ impl SampleRequestOptions {
         self.sample_clock = self.sample_clock + 1.0;
     }
     fn tone(&self, freq: f32, wave: Waveform) -> f32 {
-        use std::f32::consts::{PI, TAU};
         let period = self.sample_clock / self.sample_rate;
-        if period % 1.0 < 0.1 {
-            println!("{} {}", freq, period);
-        }
+        let phase = freq * period;
         match wave {
-            Waveform::Sine => (freq * period * TAU).sin(),
+            Waveform::Sine => (phase * TAU).sin(),
             Waveform::Square => match (freq * TAU * period).sin() {
                 i if i > 0.0 => 1.0,
                 _ => -1.0,
@@ -42,7 +52,7 @@ impl SampleRequestOptions {
 }
 
 fn main() -> Result<()> {
-    let stream = stream_setup_for(sample_next)?;
+    let stream = stream_setup()?;
     stream.play()?;
     std::io::stdin().read_line(&mut String::new())?;
     Ok(())
@@ -53,29 +63,26 @@ fn sample_next(o: &mut SampleRequestOptions, d: &SampleRequestData) -> f32 {
         0 => -1.0,
         _ => 1.0,
     } * 0.1;
-    let period = o.sample_clock / o.sample_rate;
-    let detune = (period * 10.0).sin();
     let mut output = 0.0;
-    /*output += o.tone(80.0 + detune, Waveform::Sine) * 0.5;
-    output += o.tone(160.0 + detune, Waveform::Sine);
-    output += o.tone(380.546 + offset + detune, Waveform::Triangle);
-    output += o.tone(479.458 - offset + detune, Waveform::Triangle);
-    output += o.tone(570.175 + offset + detune, Waveform::Triangle);
-    output += o.tone(718.376 + offset + detune, Waveform::Triangle);*/
-    //output * 0.1
-    o.tone(210.0 + period, Waveform::Sine) * 0.1 + o.tone(218.0, Waveform::Sine) * 0.1
+    for n in &o.notes {
+        output += (n.freq * (n.clock / o.sample_rate) * TAU).sin();
+    }
+    //output += o.tone(80.0, Waveform::Sine) * 0.5;
+    //output += o.tone(160.0 + offset, Waveform::Sine);
+    //output += o.tone(380.546 - offset, Waveform::Triangle);
+    //output += o.tone(479.458 + offset, Waveform::Triangle);
+    //output += o.tone(570.175 - offset, Waveform::Triangle);
+    //output += o.tone(718.376 + offset, Waveform::Triangle);
+    output * 0.2 / o.notes.len() as f32
 }
 
-fn stream_setup_for<F>(on_sample: F) -> Result<cpal::Stream>
-where
-    F: Fn(&mut SampleRequestOptions, &SampleRequestData) -> f32 + Send + 'static + Copy,
-{
+fn stream_setup() -> Result<cpal::Stream> {
     let (_host, device, config) = host_device_setup()?;
 
     match config.sample_format() {
-        cpal::SampleFormat::F32 => stream_make::<f32, _>(&device, &config.into(), on_sample),
-        cpal::SampleFormat::I16 => stream_make::<i16, _>(&device, &config.into(), on_sample),
-        cpal::SampleFormat::U16 => stream_make::<u16, _>(&device, &config.into(), on_sample),
+        cpal::SampleFormat::F32 => stream_make::<f32>(&device, &config.into()),
+        cpal::SampleFormat::I16 => stream_make::<i16>(&device, &config.into()),
+        cpal::SampleFormat::U16 => stream_make::<u16>(&device, &config.into()),
     }
 }
 
@@ -84,7 +91,7 @@ fn host_device_setup() -> Result<(cpal::Host, cpal::Device, cpal::SupportedStrea
 
     let device = host
         .default_output_device()
-        .ok_or(anyhow!("Default output device is not available"))?;
+        .context("Default output device is not available")?;
     println!("Output device: {}", device.name()?);
 
     let _config = device.default_output_config()?;
@@ -94,52 +101,56 @@ fn host_device_setup() -> Result<(cpal::Host, cpal::Device, cpal::SupportedStrea
     let f = formats
         .into_iter()
         .last()
-        .ok_or(anyhow!("Stream type not supported"))?;
-    let config = f.with_sample_rate(cpal::SampleRate(48000));
+        .context("Stream type not supported")?;
+    let config = f.with_max_sample_rate();
     println!("Output config: {:?}", config);
 
     Ok((host, device, config))
 }
 
-fn stream_make<T, F>(
-    device: &cpal::Device,
-    config: &cpal::StreamConfig,
-    on_sample: F,
-) -> Result<cpal::Stream>
+fn stream_make<T>(device: &cpal::Device, config: &cpal::StreamConfig) -> Result<cpal::Stream>
 where
     T: cpal::Sample,
-    F: Fn(&mut SampleRequestOptions, &SampleRequestData) -> f32 + Send + 'static + Copy,
 {
     let sample_rate = config.sample_rate.0 as f32;
     let sample_clock = 0.0;
     let nchannels = config.channels as usize;
+    let notes: Vec<Note> = vec![
+        80.0.into(),
+        160.0.into(),
+        380.546.into(),
+        479.458.into(),
+        570.175.into(),
+        718.376.into(),
+    ];
     let mut request = SampleRequestOptions {
         sample_rate,
         sample_clock,
         nchannels,
+        notes,
     };
     let err_fn = |err| eprintln!("Error building output sound stream: {}", err);
 
     let stream = device.build_output_stream(
         config,
-        move |output: &mut [T], _: &cpal::OutputCallbackInfo| {
-            on_window(output, &mut request, on_sample)
-        },
+        move |output: &mut [T], _: &cpal::OutputCallbackInfo| on_window(output, &mut request),
         err_fn,
     )?;
     Ok(stream)
 }
 
-fn on_window<T, F>(output: &mut [T], request: &mut SampleRequestOptions, on_sample: F)
+fn on_window<T>(output: &mut [T], request: &mut SampleRequestOptions)
 where
     T: cpal::Sample,
-    F: Fn(&mut SampleRequestOptions, &SampleRequestData) -> f32 + Send + 'static,
 {
     for frame in output.chunks_mut(request.nchannels) {
+        for n in &mut request.notes {
+            n.clock = (n.clock + 1.0) % (request.sample_rate / n.freq);
+        }
         request.tick();
         for (channel, sample) in frame.iter_mut().enumerate() {
             let data = SampleRequestData { channel };
-            let value: T = cpal::Sample::from::<f32>(&on_sample(request, &data));
+            let value: T = cpal::Sample::from::<f32>(&sample_next(request, &data));
             *sample = value;
         }
     }
